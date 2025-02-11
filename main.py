a691608403d3024487e6ee311053d4b7916ed920
Danny Millage <dmillage@live.com>
2025-02-10 22:06:32 -0500

    Creating a daily dle type game

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, session, redirect, Blueprint, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.get("/toggle-theme")
+def toggle_theme():
+    current_theme = session.get("theme")
+    if current_theme == "dark":
+        session["theme"] = "light"
+    else:
+        session["theme"] = "dark"
+    return redirect(request.args.get("current_page"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
